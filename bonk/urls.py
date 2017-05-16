43b712b22802412864e1d0d58ec418832f8215a0
Daniel Hokka Zakrisson <daniel@hozac.com>
2017-05-16 15:09:28 +0200

    Records can contain _
    SRV records especially.

--- bonk/urls.py
+++ bonk/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     url(r'^zone/?$', DNSZoneListView.as_view(), name='zone_list'),
     url(r'^zone/(?P<slug>[A-Za-z0-9-.]+)/?$', DNSZoneDetailView.as_view(), name='zone_detail'),
     url(r'^record/?$', DNSRecordListView.as_view(), name='record_list'),
-    url(r'^record/(?P<name>[A-Za-z0-9-.]+)/(?P<type>[A-Z]+)/?$', DNSRecordDetailView.as_view(), name='record_detail'),
+    url(r'^record/(?P<name>[A-Za-z0-9-._]+)/(?P<type>[A-Z]+)/?$', DNSRecordDetailView.as_view(), name='record_detail'),
 ]
