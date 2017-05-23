d08495b06d86c57741065e08c4a4462a733b6891
Daniel Hokka Zakrisson <daniel@hozac.com>
2017-05-23 16:21:16 +0200

    Version 0.1.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import re
 
 setup(name='bonk',
-      version='0.1.12',
+      version='0.1.13',
       description='Simple IPAM',
       author='Klarna IT Operations Core Services',
       author_email='itops.core-services@klarna.com',
