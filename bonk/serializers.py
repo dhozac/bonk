bbf5b95ead0c758c00faab9d2bfbe2a28cafc78e
Daniel Hokka Zakrisson <daniel@hozac.com>
2017-05-23 14:27:13 +0200

    Fix inverted logic

--- bonk/serializers.py
+++ bonk/serializers.py
@@ -217,12 +217,12 @@ class IPPrefixSerializer(HistorySerializerMixin):
 
 validate_mac_re = re.compile(r'^(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$')
 def validate_mac(value):
-    if validate_mac_re.match(value) is not None:
+    if validate_mac_re.match(value) is None:
         raise serializers.ValidationError("%s is not a valid MAC address (format as de:ad:be:ef:f0:00)" % value)
 
 validate_fqdn_re = re.compile(r'^(([a-zA-Z0-9_][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$')
 def validate_fqdn(value):
-    if validate_fqdn_re.match(value) is not None:
+    if validate_fqdn_re.match(value) is None:
         raise serializers.ValidationError("%s is not a valid FQDN" % value)
 
 class IPAddressSerializer(HistorySerializerMixin):
