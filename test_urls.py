cc5ccd2cb04519b71177bf5d595d5eb18e0d7a55
Daniel Hokka Zakrisson <daniel@hozac.com>
2018-03-23 17:03:40 +0100

    Include bonk URLs in the testing URLs

--- test_urls.py
+++ test_urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'', include('django_rethink.urls', namespace='django_rethink')),
+    url(r'', include('bonk.urls', namespace='bonk')),
 ]
