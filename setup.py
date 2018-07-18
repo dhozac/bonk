b716734194e051b4bb65170756af8722bd98a5b2
Daniel Hokka Zakrisson <daniel@hozac.com>
2018-07-18 15:04:44 +0200

    Version 0.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import re
 
 setup(name='bonk',
-      version='0.4.1',
+      version='0.4.2',
       license='Apache Software License',
       description='Simple IPAM',
       author='Klarna Bank AB',
