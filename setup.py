89c316893e831c2722be3d18bf40580dc4c6e8d8
Andreas Calminder <andreas.calminder@qliro.com>
2020-05-19 17:43:47 +0200

    v0.5.2 -> v0.5.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import re
 
 setup(name='bonk',
-      version='0.5.2',
+      version='0.5.3',
       license='Apache Software License',
       description='Simple IPAM',
       author='Klarna Bank AB',
